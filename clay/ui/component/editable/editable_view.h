e43bea5b57fc2ac5b6352108bbbb64d843d1e0d8
ci_lynx <ci_lynx@users.noreply.github.com>
2026-08-14 17:50:11 +0800

    [BugFix][Clay] Bridge macOS text editing shortcuts
    SkipChecks:macro

--- clay/ui/component/editable/editable_view.h
+++ clay/ui/component/editable/editable_view.h
@@ -138,6 +138,7 @@ class EditableView : public WithTypeInfo<EditableView, BaseView>,
   void UpdateEditingState(std::string text, TextSelection selection,
                           TextRange composing, Affinity affinity) override;
   void PerformAction() override;
+  bool PerformSelector(const std::string& selector) override;
 
   Transform ToGlobalTransform() const;
 
@@ -226,6 +227,8 @@ class EditableView : public WithTypeInfo<EditableView, BaseView>,
   void DoBackspace();
   void DoDelete();
   void MoveCaret(KeyCode keycode);
+  void MoveSelection(size_t position, bool modify_selection);
+  void DeleteSelectionTo(size_t position);
 
   void ToggleCaret();
   void TwinkleCaretPeriodically();
